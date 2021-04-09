396af647a8329927924933271cfed579ee7efc9f
Alivonne <Alingnue.abonge@gmail.com>
2021-04-09 01:20:52 +0000

    Restore Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,5 @@
-// This code will print on the command line.
 class HelloWorld {
    public static void main(String[] args) {
-   System.out.println("Hello, World!");
    System.out.println("Hello, World!");
    }
 }
